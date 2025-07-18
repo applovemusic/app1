49529b61c80de9878b2a67174ad913f5b9289ca7
applovemusic <applovemusic@gmail.com>
2025-07-17 22:22:51 -0400

    Update script-sw.js

--- script-sw.js
+++ script-sw.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('./app1/sw.js')
+      .register('./sw.js')
       .then(registration => {
         console.log('✅ Service Worker registrado com sucesso:', registration.scope);
 
